543dafeb05e97106c9c8c411f065936e7aa70d00
Simulanis-Dev-MohdRizwan <108863779+Simulanis-Dev-MohdRizwan@users.noreply.github.com>
2024-02-13 15:05:03 +0530

    minor changes

---
+++ Assets/Scripts/UI/CharacterSelectionPanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterSelectionPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI userName;
+
+    private void Start()
+    {
+        ShowUserName();
+    }
+
+    private void ShowUserName()
+    {
+        userName.text = DataManager.Instance.userData.name;
+    }
+}

405068ad27cd68ccba4f8c839e515a9625cc2831
Simulanis-Dev-MohdRizwan <108863779+Simulanis-Dev-MohdRizwan@users.noreply.github.com>
2024-03-04 13:54:40 +0530

    Color theme changed on main screen

--- Assets/Scripts/UI/InGameUI/messageTextHolder.cs
+++ Assets/Scripts/UI/InGameUI/messageTextHolder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,4 +7,20 @@ using UnityEngine;
 public class messageTextHolder : MonoBehaviour
 {
     public TextMeshProUGUI msgText;
+    public TextMeshProUGUI TimeText;
+
+    private void OnEnable()
+    {
+        ShowTimeInChat();
+    }
+
+    void ShowTimeInChat()
+    {
+        DateTime currentTime = System.DateTime.Now;
+        int hours = currentTime.Hour;
+        int minutes = currentTime.Minute;
+        TimeText.text = hours + ":" + minutes;
+        Debug.Log($"Current time: {hours:D2}:{minutes:D2}");
+    }
 }
+

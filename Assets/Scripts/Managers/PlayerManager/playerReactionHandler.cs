5cc752237bf5d9e95a74cfaa58caba23d435a363
Simulanis-Dev-MohdRizwan <108863779+Simulanis-Dev-MohdRizwan@users.noreply.github.com>
2024-03-06 20:24:30 +0530

    emotes fixed and done

---
+++ Assets/Scripts/Managers/PlayerManager/playerReactionHandler.cs
@@ -0,0 +1,46 @@
+using FishNet.Component.Animating;
+using FishNet.Object;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class playerReactionHandler : NetworkBehaviour
+{
+    public static playerReactionHandler Instance;
+    public bool owner;
+    private void Start()
+    {
+        Instance = this;
+        SendOwner();
+    }
+    [Button]
+    private void SendOwner()
+    {
+        if (base.IsOwner)
+        {
+            EmojiPanel.instance.TakeOwner(this.gameObject);
+        }
+    }
+
+    public void PlayReactions(string animationName)
+    {
+          PlayReactionOnServer(animationName);
+          Debug.Log("play Animation");
+    }
+    [ServerRpc(RequireOwnership = false)]
+    public void PlayReactionOnServer(string animationName)
+    {
+        PlayeReactionOnObserver(animationName);
+        Debug.Log("play Animation on server");
+    }
+
+    [ObserversRpc(BufferLast = true, ExcludeOwner = false, RunLocally = true)]
+    public void PlayeReactionOnObserver(string animationName)
+    {
+            Animator animator = GetComponent<Animator>();
+            animator.SetTrigger(animationName);
+            Debug.Log("play Animation on observer");
+            //this.GetComponent<NetworkAnimator>().Play(animationName);
+            
+    }
+
+}
--- Assets/Scripts/UI/InGameUI/EmojiPanel.cs
+++ Assets/Scripts/UI/InGameUI/EmojiPanel.cs
@@ -1,48 +1,26 @@
+using FishNet;
+using Simulanis.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EmojiPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject ownerObject;
+    //playerReactionHandler playerReactionHandler = new playerReactionHandler();
+    public static EmojiPanel instance;
+
+    private void Start()
     {
-        
+        instance = this;
+    }
+    public void playReaction(string reactionName) //will be callled on button , the string will contain the name of the trigger which will be played.
+    {
+        ownerObject.GetComponent<playerReactionHandler>().PlayReactions(reactionName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeOwner(GameObject OwnerObject)
     {
-        
-    }
-
-    public void clapEmoji() //will be called when clicked on clap Emoji
-    {
-
-    }
-
-    public void thumbsUpEmoji() //will be called when clicked on ThumbsUp Emoji
-    {
-
-    }
-
-    public void raiseHandEmoji() //will be called when clicked on Raise hand Emoji
-    {
-
-    }
-
-    public void victoryEmoji() //will be called when clicked on victory Emoji
-    {
-
-    }
-
-    public void waveEmoji() //will be called when clicked on Wave Emoji
-    {
-
-    }
-
-    public void goodEmoji() //will be called when clicked on good Emoji
-    {
-
+        ownerObject = OwnerObject;
     }
 }

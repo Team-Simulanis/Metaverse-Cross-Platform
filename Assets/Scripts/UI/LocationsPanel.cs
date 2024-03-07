e9caeffec6491462bf41d6a8cd6671da94d7f273
Simulanis-Dev-MohdRizwan <108863779+Simulanis-Dev-MohdRizwan@users.noreply.github.com>
2024-03-07 19:41:05 +0530

    teleportation feature scripts

---
+++ Assets/Scripts/UI/LocationsPanel.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LocationsPanel : MonoBehaviour //
+{
+    public static LocationsPanel instance;
+    public GameObject ownerObject;
+    private void Start()
+    {
+        instance = this;
+    }
+    public void MoveToThisLocation(Transform Location)
+    {
+        ownerObject.transform.position = Location.position;
+    }
+
+    public void TakeOwner(GameObject OwnerObject)
+    {
+        ownerObject = OwnerObject;
+    }
+}

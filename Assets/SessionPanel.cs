331020fbe8d7284304eaa8ab601f30bd5f7fd719
Simulanis-Dev-MohdRizwan <108863779+Simulanis-Dev-MohdRizwan@users.noreply.github.com>
2024-03-27 18:18:32 +0530

    Update SessionPanel.cs

--- Assets/SessionPanel.cs
+++ Assets/SessionPanel.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,16 @@ public class SessionPanel : MonoBehaviour
 {
     public SessionCard sessionPrefab;
     public Transform sessionParent;
+    public int noOfCards;
 
+    [Button]
     public void PopulateEvents()
     {
-        
+        for(int i = 0; i < noOfCards; i++) 
+        {
+            var sessionCards = Instantiate(sessionPrefab,sessionParent);
+        }
+        sessionPrefab.gameObject.SetActive(false); //remove this line later
     }
     
     
